896ca8d5a02e301dbd689125daa5aa0ffa5a62d6
waris hayat <warishayat666@gmail.com>
2026-05-22 00:58:29 +0500

    Frontend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: ['localhost', '127.0.0.1'].includes(window.location.hostname)
     ? 'http://localhost:8000' 
-    : 'https://full-stack-furniture-website.onrender.com',
+    : 'https://full-stack-furniture-website-oxz9.onrender.com',
 });
 
 
